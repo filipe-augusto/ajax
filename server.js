da18b340b4208f2ab3fc4fe61b616489ca5603f1
Filipe Augusto Santos Chaves <67299107+filipe-augusto@users.noreply.github.com>
2020-12-21 21:36:41 -0300

    att ajax

--- server.js
+++ server.js
@@ -6,5 +6,26 @@ app.use(express.static('.'))
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
-app.get('/teste',(req, res)=> res.send('Ok'))
+const multer = require('multer')//importation
+const storage = multer.diskStorage({
+    destination: function (req, file, callback){
+        callback(null,'./upload')//define a pasta
+    },
+    filename: function (req, file, callback){
+        callback(null, `${Date.now()}_${file.originalname}`)      
+    }
+})
+
+const upload = multer({storage}).single('arquivo')
+app.post('./upload',(req, res)=>{
+    upload(req, res, err => {
+        if(err){
+            return res.end('ocorreu um erro')
+        }
+        res.end('Concluido com sucesso')
+    })
+})
+
+
 app.listen(8080, ()=>console.log('executando'))
+
